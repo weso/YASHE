5547c0200b4fb6ee6883194d4113476fb95df03b
mistermboy <pabloyo97@hotmail.com>
2019-04-09 20:44:04 +0200

    Added shape reader

---
+++ src/btnScript.js
@@ -0,0 +1,42 @@
+/*
+
+const btnRDF = document.getElementById('btnRDF')
+const btnWiki = document.getElementById('btnWiki')
+const btnJapan = document.getElementById('btnJapan')
+
+var rdfShape 
+var wikiShape 
+var japanShape 
+
+$.get('./scripts/rdfBookShape.txt', function(data) {
+  rdfShape = data
+ }, 'text');
+
+ $.get('./scripts/wikidataShape.txt', function(data) {
+  wikiShape = data
+ }, 'text');
+
+ $.get('./scripts/jps.txt', function(data) {
+  japanShape = data
+ }, 'text');
+
+
+  btnRDF.addEventListener('click', function(e) {
+    // Set editor Size
+    yashe.setSize(null,"250")
+    yashe.setValue(rdfShape)
+  })
+
+  btnWiki.addEventListener('click', function(e) {
+    // Set editor Size
+    yashe.setSize(null,"600")
+    yashe.setValue(wikiShape)
+  })
+
+  btnJapan.addEventListener('click', function(e) {
+    // Set editor Size
+    yashe.setSize(null,"600")
+    yashe.setValue(japanShape)
+  })
+
+  */
--- src/main.js
+++ src/main.js
@@ -351,6 +351,47 @@ var checkEntity = function (entity){
 
 
 
+const btnRDF = document.getElementById('btnRDF')
+const btnWiki = document.getElementById('btnWiki')
+const btnJapan = document.getElementById('btnJapan')
+
+var rdfShape 
+var wikiShape 
+var japanShape 
+
+$.get('./src/rdfBookShape.txt', function(data) {
+  rdfShape = data
+ }, 'text');
+
+ $.get('./src/wikidataShape.txt', function(data) {
+  wikiShape = data
+ }, 'text');
+
+ $.get('./src/jps.txt', function(data) {
+  japanShape = data
+ }, 'text');
+
+
+  btnRDF.addEventListener('click', function(e) {
+    // Set editor Size
+    yashe.setSize(null,"250")
+    yashe.setValue(rdfShape)
+  })
+
+  btnWiki.addEventListener('click', function(e) {
+    // Set editor Size
+    yashe.setSize(null,"600")
+    yashe.setValue(wikiShape)
+  })
+
+  btnJapan.addEventListener('click', function(e) {
+    // Set editor Size
+    yashe.setSize(null,"600")
+    yashe.setValue(japanShape)
+  })
+
+
+
 
 
 
