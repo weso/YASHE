97c615bd5749e04638ff44670d4b1a34260e477c
mistermboy <pabloyo97@hotmail.com>
2019-08-08 23:51:21 +0200

    [AUTOCOMPLETERS] Added keywordsAutocomplete

--- src/autocompleters/keywords.js
+++ src/autocompleters/keywords.js
@@ -1,18 +1,16 @@
 "use strict";
 var $ = require("jquery")
-
+var yashe = require('../main.js')
 
 module.exports = function(yashe, name) {
+
+  
   return {
     isValidCompletionPosition: function() {
       return module.exports.isValidCompletionPosition(yashe);
     },
     get: function(token) {
-     
-      
-     var list = ['PREFIX','BASE']
-     return list
-            
+     return KEYWORDS
     },
     async: false,
     bulk: true,
@@ -21,13 +19,29 @@ module.exports = function(yashe, name) {
 };
 
 module.exports.isValidCompletionPosition = function(yashe) {
-
-return true
-  
-  var token = yashe.getCompleteToken();
-  var cur = yashe.getCursor()
-
-  var previousToken = yashe.getPreviousNonWsToken(cur.line, token);
-
-  
+  return true
 };
+
+
+var KEYWORDS = [
+  'BASE',
+  'PREFIX',
+  'EXTERNAL',
+  'OR',
+  'AND',
+  'NOT"',
+  'IRI',
+  'BNODE',
+  'NONLITERAL',
+  'LENGTH',
+  'MINLENGTH',
+  'MAXLENGTH',
+  'MININCLUSIVE',
+  'MINEXCLUSIVE',
+  'MAXINCLUSIVE',
+  'MAXEXCLUSIVE',
+  'TOTALDIGITS',
+  'FRACTIONDIGITS',
+  'CLOSED',
+  'EXTRA'
+  ]
