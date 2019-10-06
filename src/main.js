3be329d2d12c26c5292a3fde7dad47b684032cc3
mistermboy <pabloyo97@hotmail.com>
2019-10-06 02:21:17 +0200

    card instead of punc

--- src/main.js
+++ src/main.js
@@ -294,7 +294,25 @@ const postProcessCmElement = function(yashe) {
   checkSyntax(yashe);
 
   if (yashe.options.collapsePrefixesOnLoad) yashe.collapsePrefixes(true);
+
+
+
+  for (var l = 0; l < yashe.lineCount(); ++l) {
+    let lineTokens = getTokens(yashe.getLineTokens(l));
+    lineTokens.forEach(token =>{
+        console.log(token)
+    })
+  
+  }
+  
+
 };
+
+function getTokens(tokens){
+  return tokens.filter(function(obj){
+      return obj.type != 'ws';
+  })
+}
 
 
 /**
