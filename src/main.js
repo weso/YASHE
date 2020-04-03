70906cadc10d1fd40494e4b962e986e5917922e0
mistermboy <pabloyo97@hotmai.com>
2020-04-03 09:32:13 +0100

    semantic on shapes

--- src/main.js
+++ src/main.js
@@ -275,6 +275,11 @@ const postProcessCmElement = function(yashe) {
    * In this case, YASHE checks the sintax
    */
   yashe.on('change', function(yashe) {
+    console.log(yashe.getTokenAt(
+    { line: yashe.lastLine(), ch: yashe.getLine(yashe.lastLine()).length },
+    true
+  ).state.shapes)
+
     checkSyntax(yashe);  
   });
 
