7e479d5b20151c6d974bc051bcf198ee19bf0266
mistermboy <pabloyo97@hotmai.com>
2020-04-06 12:48:28 +0100

    working on prefixes tests

--- cypress/integration/yasheTest.js
+++ cypress/integration/yasheTest.js
@@ -1,4 +1,4 @@
-const constraintKeywordColor = 'rgb(255, 0, 0)';
+const keywordColor = 'rgb(255, 0, 0)';
 const prefixesColor = 'rgb(109, 15, 122)';
 
 describe('YASHE initialization',function() {
@@ -12,29 +12,53 @@ describe('Prefixes',function() {
     it('Type a prefix and check the defined prefixes of yashe',function() {
         cy.window().then(win => {
             win.yashe.setValue("");       
-        })
+        });
 
         cy.get('.CodeMirror textarea')
         // we use `force: true` below because the textarea is hidden
         // and by default Cypress won't interact with hidden elements
-        .type('PREFIX : <https://wwww.example.org>', { force: true })
+        .type('PREFIX : <https://wwww.example.org>', { force: true });
 
-         cy.window().then(win => {
+        cy.window().then(win => {
             let predfixes = win.yashe.getDefinedPrefixes();
             expect(Object.keys(predfixes).length).to.equals(1);  
-        })
+        });
       
     }) 
 
-    it('Check the prefixes colors are OK',function() {
-        expect(cy.get('.cm-keyword')).to.exist
-        cy.get('.cm-keyword').should('have.css', 'color', constraintKeywordColor)
-        expect(cy.get('.cm-prefixDelcIRI')).to.exist
-        cy.get('.cm-prefixDelcIRI').should('have.css', 'color', prefixesColor)
-        expect(cy.get('.cm-prefixDelcAlias')).to.exist
-        cy.get('.cm-prefixDelcAlias').should('have.css', 'color', prefixesColor)
-        
+    it('Check the prefixes classes exist after typing a prefix',function() {
+
+        cy.window().then(win => {
+            win.yashe.setValue("");       
+        });
+
+        cy.get('.cm-keyword').should('not.exist');
+        cy.get('.cm-prefixDelcIRI').should('not.exist');
+        cy.get('.cm-prefixDelcAlias').should('not.exist');
+
+        cy.get('.CodeMirror textarea')
+        .type('PREFIX : <https://wwww.example.org>', { force: true });
+
+        cy.get('.cm-keyword').should('exist');
+        cy.get('.cm-prefixDelcIRI').should('exist');
+        cy.get('.cm-prefixDelcAlias').should('exist');
     })
+
+    it('Check the prefixes colors are OK',function() {
+        cy.get('.cm-keyword').should('have.css', 'color', keywordColor);
+        cy.get('.cm-prefixDelcIRI').should('have.css', 'color', prefixesColor);
+        cy.get('.cm-prefixDelcAlias').should('have.css', 'color', prefixesColor);
+    })
+
+
+    it('Check the prefixes autocompleter',function() {
+        cy.get('.CodeMirror-hints').should('not.exist');
+        cy.get('.CodeMirror textarea')
+        .type('\nPREFIX wd', { force: true })
+        cy.get('.CodeMirror-hints').should('exist');
+    })
+
+
 })
 
 
