bbd4c1c550f8fa0e3d48d52989fd7a1017795948
mistermboy <pabloyo97@hotmail.com>
2020-04-07 15:44:22 +0100

    Another wikibase instance entity autocompleter test

--- cypress/integration/wikibase.js
+++ cypress/integration/wikibase.js
@@ -7,7 +7,7 @@ describe('YASHE initialization',function() {
 
 describe('Wikibase',function() {
     
-    it('Check wikibase enity autocompleter works with the wikidata instance',function() {
+    it('Check wikibase entity autocompleter works with the wikidata instance',function() {
         clearYashe();
         
         cy.get('.CodeMirror-hints').should('not.exist');
@@ -28,6 +28,28 @@ describe('Wikibase',function() {
         
         cy.window().then(win => {
             expect(win.yashe.getValue()).to.equals("PREFIX wd: <http://www.wikidata.org/entity/> \nwd:Q1");       
+        });
+    }) 
+
+    it('Check wikibase entity autocompleter works with another wikibase instance',function() {
+        clearYashe();
+        
+        cy.get('.CodeMirror-hints').should('not.exist');
+        //Activate the autcompleter
+        cy.get('.CodeMirror textarea')
+        .type('PREFIX labra: <https://cursoslabra.wiki.opencura.com/wiki/Item:> \nlabra:{ctrl}{ }', { force: true });
+        cy.get('.CodeMirror-hints').should('exist');
+        cy.get('.CodeMirror-hints').should('have.text',"Type to search for an entity");
+
+        //Type some entity to search and click the first one
+        cy.get('.CodeMirror textarea')
+        .type('person', { force: true });
+       
+        cy.wait(500)
+        cy.get('.CodeMirror-hints').children().first().click();
+        
+        cy.window().then(win => {
+            expect(win.yashe.getValue()).to.equals("PREFIX labra: <https://cursoslabra.wiki.opencura.com/wiki/Item:> \nlabra:Q1");       
         });
     }) 
 
