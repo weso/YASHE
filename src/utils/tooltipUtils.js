4195882af53ab81c58b8906f1ac7c51601c3b37a
mistermboy <pabloyo97@hotmail.com>
2020-04-08 09:08:42 +0100

    fixed theme tooltip bug

--- src/utils/tooltipUtils.js
+++ src/utils/tooltipUtils.js
@@ -107,6 +107,11 @@ var loadTooltip = function(yashe,data,wikiElement,posX,posY){
       }
 
       theme = yashe.getOption('theme');
+      let cssStyle = themeStyles['default'];
+      if(theme=='dark'){
+        cssStyle = themeStyles['dark'];
+      }
+
       //Jquery in 2020 coooool
       $( '<div class="CodeMirror cm-s-default CodeMirror-wrap">' )
         .css( 'position', 'absolute' )
@@ -117,7 +122,7 @@ var loadTooltip = function(yashe,data,wikiElement,posX,posY){
         } )
         .addClass('wikidataTooltip').css('height','auto')
         .append(
-          $('<div class="wikidata_tooltip">').css(themeStyles[theme])
+          $('<div class="wikidata_tooltip">').css(cssStyle)
           .append(
             $('<div>').html(entity).css(styles.title))
           .append(
@@ -195,7 +200,7 @@ const styles ={
 }
 
 const themeStyles ={
-  wiki:{
+  default:{
     'display': 'inline-block',
     'justify-content': 'center',
     'padding': '10px',
@@ -223,5 +228,5 @@ module.exports = {
   grammarTootlip:grammarTootlip,
   triggerTooltip:triggerTooltip,
   removeWikiToolTip:removeWikiToolTip,
-  debounce:debounce
+  debounce:debounce,
 };
