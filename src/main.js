1f5ee5d16f20061eb1fd1eb0fb06b4c99371e623
mistermboy <pabloyo97@hotmail.com>
2020-06-25 17:56:05 +0100

    ?

--- src/main.js
+++ src/main.js
@@ -381,6 +381,7 @@ root.storeContent = function(yashe) {
 
 
 root.prettify = function(yashe){
+  console.log('eeeo')
   if(!yashe.hasErrors()){
     prettyUtils.prettify(yashe);
     if(yashe.hasErrors())yashe.undo(); //Just in case the prettify fail
