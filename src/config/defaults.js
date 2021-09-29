c4d1b50f2aa357f708f6bc290ed8081808b3ee90
mistermboy <pabloyo97@hotmail.com>
2021-09-29 13:39:46 +0200

    restored default shape (now yes xd)

--- src/config/defaults.js
+++ src/config/defaults.js
@@ -13,14 +13,15 @@ YASHE.defaults = $.extend(true, {}, YASHE.defaults, {
 	 *  Default shape 
 	 */
    value:  `PREFIX :       <http://example.org/>
-   PREFIX schema: <http://schema.org/>
-   PREFIX xsd:  <http://www.w3.org/2001/XMLSchema#>
-   :User {
-     schema:name          xsd:string  @:User ;
-     schema:birthDate     xsd:date?  ;
-     schema:gender        [ schema:Male schema:Female ] OR xsd:string ;
-     schema:knows         IRI @:User*
-   }
+PREFIX schema: <http://schema.org/>
+PREFIX xsd:  <http://www.w3.org/2001/XMLSchema#>
+
+:User {
+  schema:name          xsd:string  @:User ;
+  schema:birthDate     xsd:date?  ;
+  schema:gender        [ schema:Male schema:Female ] OR xsd:string ;
+  schema:knows         IRI @:User*
+}
    `,
   highlightSelectionMatches: {
     showToken: /\w/
