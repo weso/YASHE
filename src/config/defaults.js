3f8024481bab3ceb4bf9dc6cd0db01ff6315aee7
Pablo Menéndez ◔_◔ <pabloyo97@hotmail.com>
2021-10-19 13:27:27 +0200

    Update defaults.js

--- src/config/defaults.js
+++ src/config/defaults.js
@@ -17,7 +17,7 @@ PREFIX schema: <http://schema.org/>
 PREFIX xsd:  <http://www.w3.org/2001/XMLSchema#>
 
 :User {
-  schema:name          xsd:string  @:User ;
+  schema:name          xsd:string ;
   schema:birthDate     xsd:date?  ;
   schema:gender        [ schema:Male schema:Female ] OR xsd:string ;
   schema:knows         IRI @:User*
